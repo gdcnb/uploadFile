ba6381dc23293034215f834127632067fdac0aa2
chennb <4013531@qq.com>
2014-04-27 23:07:43 +0800

    [ADD] 添加 formidable 模块

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var formidable = require('formidable');
 var fs = require('fs');
 
 /* GET users listing. */
